e503462d6a6f4b1c4be41e9e439182dbcaed3cb0
Kevin <kgwiazdowski88@gmail.com>
2021-09-04 14:55:45 +0200

    Create Header component and add react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import Header from "./Header";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Header />
+          </Route>
+          <Route path="/collection" exact>
+            <Header />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
