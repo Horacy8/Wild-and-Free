9e643956a74b0384bdc302967d2d92ca55be2e82
Kevin <kgwiazdowski88@gmail.com>
2021-10-19 13:03:09 +0200

    Add fetchData with axios. Create Loading and Message box

--- frontend/src/components/Collection.js
+++ frontend/src/components/Collection.js
@@ -1,19 +1,46 @@
-import React, { useEffect } from "react";
-import data from "../data";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Product from "./Product";
 import "./Collection.css";
+import LoadingBox from "./LoadingBox";
+import MessageBox from "./MessageBox";
 
 function Collection() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get("/api/products");
+        setLoading(false);
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+    fetchData();
+
     window.scroll(0, 0);
   }, []);
 
   return (
-    <div className="collection">
-      <h2 className="collection__title">Kolekcja</h2>
-      {data.products.map((product) => (
-        <Product key={product._id} product={product} />
-      ))}
+    <div>
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox>{error}</MessageBox>
+      ) : (
+        <div className="collection">
+          <h2 className="collection__title">Kolekcja</h2>
+          {products.map((product) => (
+            <Product key={product._id} product={product} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
---
+++ frontend/src/components/LoadingBox.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./LoadingBox.css";
+
+function LoadingBox() {
+  return (
+    <div className="loading-box">
+      <i className="fa fa-spinner fa-spin"></i>
+    </div>
+  );
+}
+
+export default LoadingBox;
