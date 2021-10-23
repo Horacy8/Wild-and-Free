eb1ddf1e908faafcd8e16008d09e4f172ae06819
Kevin <kgwiazdowski88@gmail.com>
2021-10-23 17:13:03 +0200

    Create Products in MongoDB

--- backend/data.js
+++ backend/data.js
@@ -1,7 +1,6 @@
 const data = {
   products: [
     {
-      _id: "1",
       name: "Wyczesana sukienka",
       category: "dress",
       image: "/img/dress1.jpg",
@@ -10,7 +9,6 @@ const data = {
       description: "high quality product",
     },
     {
-      _id: "2",
       name: "Klawa sukienka",
       category: "dress",
       image: "/img/dress2.jpg",
@@ -19,7 +17,6 @@ const data = {
       description: "high quality product",
     },
     {
-      _id: "3",
       name: "Jaha",
       category: "dress",
       image: "/img/dress3.jpg",
@@ -28,7 +25,6 @@ const data = {
       description: "high quality product",
     },
     {
-      _id: "4",
       name: "Na wprośki",
       category: "pants",
       image: "/img/pants1.jpg",
@@ -37,7 +33,6 @@ const data = {
       description: "high quality product",
     },
     {
-      _id: "5",
       name: "Bumela",
       category: "hoodie",
       image: "/img/hoodie1.jpg",
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
 import data from "./data.js";
+import productRouter from "./routers/productRouter.js";
 
 const app = express();
-
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product not Found" });
+mongoose.connect(
+  process.env.MONGODB_URL || "mongodb://localhost:27017/narave",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   }
-});
+);
 
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
+app.use("/api/products", productRouter);
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
